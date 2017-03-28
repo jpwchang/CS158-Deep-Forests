b6e48b184b51ebd4a37a208676692a9968ce2076
scchow <schow@hmc.edu>
2017-03-28 14:08:09 -0700

    Added tfidf cache file and start of visualize file

---
+++ visualize.py
@@ -0,0 +1,9 @@
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from glob import iglob
+from bs4 import BeautifulSoup
+import pandas as pd
+import numpy as np
+import os
+
+from constants import *
+
