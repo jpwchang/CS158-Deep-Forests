b8eeea2505b976de29a3cdfccbff062abf468e0e
Jonathan Chang <jonathan.chang13@gmail.com>
2017-03-28 13:38:46 -0700

    fix missing numpy dependency

--- load_data.py
+++ load_data.py
@@ -2,6 +2,7 @@ from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from glob import iglob
 from bs4 import BeautifulSoup
 import pandas as pd
+import numpy as np
 import os
 
 from constants import *
