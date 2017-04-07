f47e15709d28810b223c101f23c9706788c69c7e
Jonathan Chang <jonathan.chang13@gmail.com>
2017-04-07 16:15:11 -0700

    Subsample data to make it possible to run gcForest without memory running out

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import sys
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_selection import SelectKBest
+
+from gcForest.GCForest import gcForest
 
 from load_data import load_data
 from baseline import TitleFinder
@@ -14,6 +17,11 @@ def main():
         sys.exit(1)
 
     X,y,tfidf = load_data()
+
+    # feature selection to make the problem tractable for gcforest
+    fs = SelectKBest(k=100)
+    X = fs.fit_transform(X,y)
+    
     X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.6, random_state=1337, stratify=y)
     
     model = None
@@ -23,6 +31,8 @@ def main():
         model = AuthorFinder(tfidf.get_feature_names()) 
     elif sys.argv[1] == "forest":
         model = RandomForestClassifier()
+    elif sys.argv[1] == "deepforest":
+        model = gcForest(shape_1X=100)
 
     model.fit(X_train,y_train)
     y_pred = model.predict(X_test)   
