d53ec301a576fec66557082feee623638c3d6427
scchow <schow@hmc.edu>
2017-04-10 15:54:58 -0700

    Added basic gridsearch code for gcForests

---
+++ gridsearch_gcForest.py
@@ -0,0 +1,57 @@
+import sys
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_selection import SelectKBest
+
+from gcForest.GCForest import gcForest
+
+from load_data import load_data
+from baseline import TitleFinder
+from baseline_author import AuthorFinder
+from constants import *
+
+def main():
+
+    X, y, tfidf = load_data()
+
+    # Number of Features
+    print("Using ", NUM_FEATURES, "Features based on tf-idf")
+
+    # feature selection to make the problem tractable for gcforest
+    fs = SelectKBest(k=NUM_FEATURES)
+    X = fs.fit_transform(X,y)
+    X = np.asarray(X.todense())
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.6, random_state=1337, stratify=y)
+    
+    possibleNumTrees = []
+    possibleNumForests = []
+
+    bestAccuracy = -float("inf")
+    bestNumTrees = 0
+    bestNumForests = 0
+
+    for numForests in possibleNumForests:
+        for numTrees in possibleNumTrees:
+            model = gcForest(shape_1X=NUM_FEATURES, n_cascadeRF=numForests, n_cascadeRFtree=numTrees, n_jobs=-1)
+            model.fit(X_train,y_train)
+            y_pred = model.predict(X_test)
+            accuracy = accuracy_score(y_test, y_pred)
+            print(confusion_matrix(y_test, y_pred))
+            print(classification_report(y_test, y_pred))
+            print("Accuracy:", accuracy)
+
+            if accuracy > bestAccuracy:
+                bestAccuracy = accuracy
+                bestNumTrees = numTrees
+                bestNumForests = numForests
+    
+    print("Best Accuracy = ", bestAccuracy)
+    print("best Num Forests =". bestNumForests)
+    print("Best Num Trees =", bestNumTrees)
+
+if __name__ == '__main__':
+    main()
