24dd10d0921db1b3a071ed41bac2202c6955283f
Jonathan Chang <jonathan.chang13@gmail.com>
2017-04-10 15:02:29 -0700

    Revert slicing

--- constants.py
+++ constants.py
@@ -1,3 +1,3 @@
 import os
 DATA_PATH = os.path.join(".","amazon_book_reviews")
-NUM_FEATURES = 100
+NUM_FEATURES = 500
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import numpy as np
 
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
@@ -23,7 +24,7 @@ def main():
     fs = SelectKBest(k=NUM_FEATURES)
     X = fs.fit_transform(X,y)
     
-    X_train, X_test, y_train, y_test = train_test_split(X.todense(), y, train_size=0.6, random_state=1337, stratify=y)
+    X_train, X_test, y_train, y_test = train_test_split(np.asarray(X.todense()), y, train_size=0.6, random_state=1337, stratify=y)
     
     model = None
     if sys.argv[1] == "baseline":
@@ -31,10 +32,9 @@ def main():
     elif sys.argv[1] == "author":
         model = AuthorFinder(tfidf.get_feature_names()) 
     elif sys.argv[1] == "forest":
-        model = RandomForestClassifier(n_jobs=-1)
+        model = RandomForestClassifier(n_estimators=30, n_jobs=-1)
     elif sys.argv[1] == "deepforest":
-        model = gcForest(shape_1X=NUM_FEATURES, window=[NUM_FEATURES // 16, NUM_FEATURES // 9, NUM_FEATURES // 4],
-                         n_cascadeRFtree=1000, n_jobs=-1)
+        model = gcForest(shape_1X=NUM_FEATURES, n_cascadeRF=4, n_cascadeRFtree=30, n_jobs=-1)
 
     model.fit(X_train,y_train)
     y_pred = model.predict(X_test)   
